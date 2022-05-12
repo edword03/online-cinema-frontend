84e44f22f03048e9fe1abacd747725160e328f52
Dmitrii Veretennikov <dima_veretennikov@list.ru>
2022-05-12 23:45:05 +0400

    add configs of prettier, tailwind css, next, ts

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	optimizeFonts: true,
+	env: {
+		APP_URL: process.env.REACT_APP_URL,
+		APP_ENV: process.env.REACT_APP_ENV,
+	},
+	async rewrites() {
+		return [
+			{
+				source: '/api/:path*',
+				destination: 'https://localhost:4000/api/:path*',
+			},
+			{
+				source: '/uploads/:path*',
+				destination: 'https://localhost:4000/uploads/:path*',
+			},
+		];
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
