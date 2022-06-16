0b362ecc92145afa49fa82ab7450f4fefddf9bda
Dmitrii Veretennikov <dima_veretennikov@list.ru>
2022-06-17 01:22:42 +0400

    feat: add favicons and progress bar

---
+++ app/constants/common.ts
@@ -0,0 +1,6 @@
+export const accentColor = '#E30013';
+export const bgColor = '#191B1F';
+export const themeColor = '#181B1F';
+
+export const IS_SERVER = typeof window === 'undefined';
+export const IS_CLIENT = typeof window !== 'undefined';
---
+++ app/providers/HeadProvider/HeadProvider.tsx
@@ -0,0 +1,35 @@
+import Head from 'next/head';
+import NextNProgress from 'nextjs-progressbar';
+import { FC } from 'react';
+
+import { accentColor, themeColor } from '@/constants/common';
+
+import { Favicons } from './Favicons';
+
+export const HeadProvider: FC = () => {
+	return (
+		<>
+			<NextNProgress
+				color={accentColor}
+				startPosition={0.3}
+				stopDelayMs={200}
+				height={3}
+			/>
+			<Head>
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1, maximum-scale=1.0"
+				/>
+
+				<Favicons />
+
+				<meta name="theme-color" content={themeColor} />
+				<meta name="msapplication-navbutton-color" content={themeColor} />
+				<meta
+					name="apple-mobile-web-app-status-bar-style"
+					content={themeColor}
+				/>
+			</Head>
+		</>
+	);
+};
