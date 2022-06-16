792f8a549fe3a090abec26a071e5ae46a9fac689
Dmitrii Veretennikov <dima_veretennikov@list.ru>
2022-06-16 16:45:56 +0400

    feat: add 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,12 @@
+import { Meta } from '@/components/SEO/Meta';
+
+import { Heading } from '@/ui/Heading/Heading';
+
+export default function Error404() {
+	return (
+		<>
+			<Meta title="Page not found" />
+			<Heading title="404 - Page not found" />
+		</>
+	);
+}
