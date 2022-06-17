dc365442e0b8d8804c590daf1eeab0d7fb3bbb8d
Dmitrii Veretennikov <dima_veretennikov@list.ru>
2022-06-17 12:56:13 +0400

    update: updated package.json, package-lock.json and other

--- app/components/screens/home/index.tsx
+++ app/components/screens/home/index.tsx
@@ -1,15 +1,18 @@
-import { Meta } from "@/components/SEO/Meta"
-import { Heading } from "@/components/ui/Heading/Heading"
+import { Meta } from '@/components/SEO/Meta';
+import { Heading } from '@/components/ui/Heading/Heading';
 
 export const Home = () => {
-  return (
-    <>
-      <Meta
+	return (
+		<>
+			<Meta
 				title="Watch movies online"
 				description="Watch movies and TV shows online"
 				isPublic
 			/>
-      <Heading title="Watch movies online" className="text-gray-300 mb-8 text-xl" />
-    </>
-  )
-}
+			<Heading
+				title="Watch movies online"
+				className="text-gray-300 mb-8 text-xl"
+			/>
+		</>
+	);
+};
