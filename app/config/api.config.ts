789131506d370f611d2f4abff1965e90b8868cfa
edword03 <dima_veretennikov@list.ru>
2022-07-03 22:55:30 +0300

    fix: other changes in tsconfig, cconfig

--- app/config/api.config.ts
+++ app/config/api.config.ts
@@ -2,7 +2,7 @@ export const API_URL = `${process.env.APP_URL}/api`;
 
 export const getAuthUrl = (path: string) => `${API_URL}/auth/${path}`;
 export const getGenresUrl = (path: string) => `${API_URL}/genres/${path}`;
-export const getUserUrl = (path: string) => `/users/${path}`;
+export const getUsersUrl = (path: string) => `${API_URL}/users/${path}`;
 export const getMoviesUrl = (path: string) => `${API_URL}/movies/${path}`;
 export const getActorsUrl = (path: string) => `/actors/${path}`;
 export const getRatingUrl = (path: string) => `/rating/${path}`;
