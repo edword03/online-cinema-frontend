88736e8d55b1d30ebd13042bd785d6d2247b384f
edword03 <dima_veretennikov@list.ru>
2022-07-04 12:36:30 +0300

    refactor: refactored getting api url

--- app/config/api.config.ts
+++ app/config/api.config.ts
@@ -1,8 +1,10 @@
 export const API_URL = `${process.env.APP_URL}/api`;
 
-export const getAuthUrl = (path: string) => `${API_URL}/auth/${path}`;
-export const getGenresUrl = (path: string) => `${API_URL}/genres/${path}`;
-export const getUsersUrl = (path: string) => `${API_URL}/users/${path}`;
-export const getMoviesUrl = (path: string) => `${API_URL}/movies/${path}`;
-export const getActorsUrl = (path: string) => `/actors/${path}`;
-export const getRatingUrl = (path: string) => `/rating/${path}`;
+const getApiEndpoint = (endpoint: string) => `${API_URL}/${endpoint}`;
+
+export const getAuthUrl = (path: string) => getApiEndpoint(`auth/${path}`);
+export const getGenresUrl = (path: string) => getApiEndpoint(`genres/${path}`);
+export const getUsersUrl = (path: string) => getApiEndpoint(`users/${path}`);
+export const getMoviesUrl = (path: string) => getApiEndpoint(`movies/${path}`);
+export const getActorsUrl = (path: string) => getApiEndpoint(`actors/${path}`);
+export const getRatingUrl = (path: string) => getApiEndpoint(`rating/${path}`);
