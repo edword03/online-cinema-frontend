261458912ba0737d7dd7265e8d59751dd4ee63b3
edword03 <dima_veretennikov@list.ru>
2022-07-04 12:37:16 +0300

    feat: add getGenresString method in utils

--- app/utils/movie/getGenreString.ts
+++ app/utils/movie/getGenreString.ts
@@ -1,2 +1,9 @@
 export const getGenreString = (index: number, lenght: number, name: string) =>
 	index + 1 === lenght ? name : name + ', ';
+
+interface IArrayItem {
+	name: string;
+}
+
+export const getGenresList = (array: IArrayItem[]) =>
+	array.map((i) => i.name).join(', ');
